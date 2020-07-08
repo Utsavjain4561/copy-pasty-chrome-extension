4dfb4322b4acfcede44c82071db3ba82153b0a3e
UTSAV JAIN@LAPTOP-AMVGD54G <utsavslife@gmail.com>
2020-07-08 14:51:00 +0530

    add storage properties

--- background.js
+++ background.js
@@ -1,40 +1,25 @@
 console.log("Background is running")
-let copiedData=[];
-const  getClipboardContent=()=>{
-    let backgroundPage =chrome.extension.getBackgroundPage();
-    backgroundPage.document.body.innerHTML="";
-    let helperDiv = backgroundPage.document.createElement("div");
-    document.body.appendChild(helperDiv);
-    helperDiv.contentEditable = true;
-
-    let range = document.createRange();
-    range.selectNode(helperDiv);
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
-    helperDiv.focus();
-
-    backgroundPage.document.execCommand("Paste");
-    return helperDiv.innerHTML;
-}
-
+let clipboard=[];
 // Lsitener to recieve message from content script
 let index = 0;
+chrome.storage.onChanged.addListener(()=>{
+    console.log("Storage is changed");
+    chrome.storage.local.get(['word'],(text)=>{
+        clipboard.push(text.word);
+        
+    });
+    console.log(clipboard);
+
+})
+
+
 // Lsitener to recieve message from content script
 chrome.commands.onCommand.addListener(function(command) {
   console.log('onCommand event received for message: ', command);
   console.log(index);
-  console.log(copiedData[index]);
-  index = (index+1)%copiedData.length;
+  console.log(clipboard[index]);
+  index = (index+1)%clipboard.length;
 });
 
-chrome.runtime.onMessage.addListener(recieveText);
-function recieveText(req,send,res){
-    const {event} = req;
-    if(event==='copy'){
-        //store the message in the global list
-       
-        copiedData.push(getClipboardContent());
-    }
-    console.log("Copied Data is ",copiedData)
-}
+
 
--- content.js
+++ content.js
@@ -1,17 +1,13 @@
 console.log("copy-pasty-chrome-extension");
 
+
 // Listener will trigger at CTRL+C or mouse copy event
 document.addEventListener('copy',function(event){
     console.log("Content copied");
 
     //Add selected data to the clipboard
     let selectedWord = window.getSelection().toString();
-    event.clipboardData.setData('text/plain',selectedWord);
-    event.preventDefault();
-    chrome.runtime.sendMessage({
-        event:"copy",
-    });
-    
+    chrome.storage.local.set({word:selectedWord},()=>console.log("Value is saved to local storage"));
 })  
 
 
