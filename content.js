21caef650db530bf87fa9fde1113268a51d586de
UTSAV JAIN@LAPTOP-AMVGD54G <utsavslife@gmail.com>
2020-07-08 21:54:39 +0530

    add paste text

--- content.js
+++ content.js
@@ -1,5 +1,18 @@
 console.log("copy-pasty-chrome-extension");
-
+let index=0,
+	currentFocus;
+const getFocussedTarget = (event)=>event.target;
+const pasteCurrentText = (currentText)=>{
+	console.log(currentText);
+	if(currentFocus instanceof HTMLInputElement){
+		
+		currentFocus.value = currentFocus.value+' '+currentText;
+	}
+	else if(currentFocus.isContentEditable){
+		let content = document.createTextNode(currentText);
+		currentFocus.appendChild(content);
+	}
+}
 
 // Listener will trigger at CTRL+C or mouse copy event
 document.addEventListener('copy',function(event){
@@ -19,20 +32,28 @@ document.addEventListener('copy',function(event){
     });
 });
 
-window.onkeydown = keyPressed;
-function keyPressed(event){
-	let key = event.key;
+window.onkeydown =(event)=>{
+	let key = event.key,
+		localList = [];
+	console.log("Key is ",typeof key)
 	if (key==="Control")
 		return;
-	 if (event.ctrlKey&&key==='.') {
-         console.log("Key pressed");
+	
+	 if (event.ctrlKey&&key===".") {
+        console.log("Triggered");
     // Even though event.key is not 'Control' (e.g., 'a' is pressed),
     chrome.storage.local.get(['list'],function(result){
-    	console.log('hello');
-    	console.log(result.list);
-    })
-  } else {
-    // alert(`Key pressed ${key}`);
-  }
-	// console.log(key);
+		localList = result.list;
+		pasteCurrentText(localList[index]);
+		index++;
+		index%=localList.length;
+	})
+	
+  } 
+}
+
+window.onmousedown = (event)=>{
+	console.log("Clicked");
+	currentFocus = getFocussedTarget(event);
+	console.log("Focus changed to ",currentFocus);
 }
