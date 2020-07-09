3da0beba129b66f1778b2a66c03f667bcb9dc8ec
UTSAV JAIN@LAPTOP-AMVGD54G <utsavslife@gmail.com>
2020-07-09 19:49:45 +0530

    add clipboard update

--- content.js
+++ content.js
@@ -1,28 +1,33 @@
 let _previousData="";
-const setClipboardText =(clipText)=>{
+
+const setClipboardText = async (clipText)=>{
     chrome.storage.local.get(['list'],clipboard=>{
         let {list} = clipboard;
         if(typeof list === "undefined")
-            list = [];
-        list.push(clipText);
-        chrome.storage.local.set({'list':list},status=>{});
+			list = [];
+		if(list.indexOf(clipText)==-1)
+			list.push(clipText);
+        chrome.storage.local.set({'list':list},status=>console.log("Text Saved"));
     })
 }
-const getClipboardText = ()=>{
+const getClipboardText = async()=>{
 	chrome.storage.local.get(['list'],clipboard=>{
 		console.log(clipboard.list);
 	})
 }
+
+
 setInterval(()=>{
     console.log("Checking for clipboard changes.....");
+    
     navigator.clipboard.readText()
     .then(clipboardText=>{
         if(clipboardText.length>0 && clipboardText!==_previousData){
             //Add to this text to the storage
 			setClipboardText(clipboardText);
-			getClipboardText();
-            _previousData = clipboardText;
+            _previousData = clipboardText
         }
     })
+    .then(()=>getClipboardText())
     .catch(err=>console.log(err))
 },2000)
