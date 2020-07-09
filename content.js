0e171c6f11eb0ec76e4584a398f014b105af62d2
UTSAV JAIN@LAPTOP-AMVGD54G <utsavslife@gmail.com>
2020-07-09 18:05:59 +0530

    navigator added

--- content.js
+++ content.js
@@ -1,72 +1,28 @@
-
-let index = 0,
-	currentFocus;
-	
-console.log("copy-pasty-chrome-extension");
-const getFocussedTarget = (event)=>event.target;
-const recieveText=(req,send,res)=>{
-	let {event} = req;
-	index = req.index;
-    console.log('message recieveText');
-    //store the message in the global list
-    window.onkeyup = keyReleased;
-    return true;
+let _previousData="";
+const setClipboardText =(clipText)=>{
+    chrome.storage.local.get(['list'],clipboard=>{
+        let {list} = clipboard;
+        if(typeof list === "undefined")
+            list = [];
+        list.push(clipText);
+        chrome.storage.local.set({'list':list},status=>{});
+    })
 }
-const pasteCurrentText = (currentText)=>{
-	console.log(currentText);
-	if(currentFocus instanceof HTMLInputElement){
-		
-		currentFocus.value = currentFocus.value+' '+currentText;
-	}
-	else if(currentFocus.isContentEditable){
-		let content = document.createTextNode(currentText);
-		currentFocus.appendChild(content);
-	}
-}
-const keyReleased=(event)=>{
-	let key = event.key;
-	if (key==="Control"){
-		console.log("control released!!");
-		chrome.storage.local.get(['list'],(result)=>{
-			pasteCurrentText(result.list[index]);             
-		})
-		
-	}
-	
-	chrome.storage.local.get(['list'],(result)=>{
-		currentFocus.placeholder = currentFocus.value+result.list[index]
+const getClipboardText = ()=>{
+	chrome.storage.local.get(['list'],clipboard=>{
+		console.log(clipboard.list);
 	})
-	console.log(key);
-	return true;
 }
-
-window.onmousedown = (event)=>{
-	console.log("Clicked");
-	currentFocus = getFocussedTarget(event);
-	console.log("Focus changed to ",currentFocus);
-}
-
-// Listener will trigger at CTRL+C or mouse copy event
-document.addEventListener('copy',function(event){
-    // console.log("Content copied");
-
-    //Add selected data to the clipboard
-    let selectedWord = window.getSelection().toString();
-    chrome.storage.local.get(['list'],function(result){
-    	let localList = result.list;
-    	console.log(typeof localList);
-    	if (typeof localList === 'undefined')
-    		localList = [];
-    	localList.push(selectedWord);
-    	chrome.storage.local.set({'list':localList},function(arg){
-    		console.log(localList);
-    		return true;
-    	});
-    	return true;
-    });
-    return true;
-});
-
-chrome.runtime.onMessage.addListener(recieveText);
-
-
+setInterval(()=>{
+    console.log("Checking for clipboard changes.....");
+    navigator.clipboard.readText()
+    .then(clipboardText=>{
+        if(clipboardText.length>0 && clipboardText!==_previousData){
+            //Add to this text to the storage
+			setClipboardText(clipboardText);
+			getClipboardText();
+            _previousData = clipboardText;
+        }
+    })
+    .catch(err=>console.log(err))
+},2000)
