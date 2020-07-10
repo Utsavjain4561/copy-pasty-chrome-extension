56c4d1e0d115d70ddeef4534ef5221310a7a3034
Netfreak21 <34313022+Netfreak21@users.noreply.github.com>
2020-07-10 18:35:55 +0530

    popup MRU implemented

--- content.js
+++ content.js
@@ -1,54 +1,30 @@
-console.log("copy-pasty-chrome-extension");
-
-// Listener will trigger at CTRL+C or mouse copy event
-document.addEventListener('copy',function(event){
-    // console.log("Content copied");
-
-    //Add selected data to the clipboard
-    let selectedWord = window.getSelection().toString();
-    chrome.storage.local.get(['list'],function(result){
-    	let localList = result.list;
-    	console.log(typeof localList);
-    	if (typeof localList === 'undefined')
-    		localList = [];
-    	localList.push(selectedWord);
-    	chrome.storage.local.set({'list':localList},function(arg){
-    		console.log(localList);
-    		return true;
-    	});
-    	return true;
-    });
-    return true;
-});
-
-chrome.runtime.onMessage.addListener(recieveText);
-function recieveText(req,send,res){
-    let {event,index} = req;
-    	console.log('message recieveText');
-        //store the message in the global list
-        window.onkeyup = keyReleased;
-    return true;
+let _previousData="";
+let _maxListSize = 5;
+const setClipboardText = async (clipText)=>{
+    chrome.storage.local.get(['list'],clipboard=>{
+        let {list} = clipboard;
+        if(typeof list === "undefined")
+            list = [];
+		if(list.indexOf(clipText)==-1)
+			list.push(clipText);
+        chrome.storage.local.set({'list':list},status=>console.log("Text Saved"));
+    })
 }
-// window.onkeydown = keyPressed;
-// function keyPressed(event){
-// 	let key = event.key;
-// 	if (key==="Control")
-// 		return;
-// 	 if (event.ctrlKey&&key==='I') {
-//     // Even though event.key is not 'Control' (e.g., 'a' is pressed),
-//     chrome.storage.local.get(['list'],function(result){
-//     	console.log('hello');
-//     	console.log(result.list);
-//     })
-//   } else {
-//     // alert(`Key pressed ${key}`);
-//   }
-	// console.log(key);
-// }
-function keyReleased(event){
-	let key = event.key;
-	if (key==="Control")
-		console.log("control released!!");
-	console.log(key);
-	return true;
-}
+
+
+
+
+setInterval(()=>{
+    console.log("Checking for clipboard changes.....");
+    
+    navigator.clipboard.readText()
+    .then(clipboardText=>{
+        if(clipboardText.length>0 && clipboardText!==_previousData){
+            //Add to this text to the storage
+			setClipboardText(clipboardText);
+            _previousData = clipboardText
+        }
+
+    })
+    .catch(err=>console.log(err))
+},2000)
---
+++ popup.js
@@ -0,0 +1,39 @@
+console.log("Popup is running");
+let _clipboardList = document.querySelector("#clipboard_list");
+function addClipboardListItem(text){
+    console.log("Text is ",text);
+    console.log(_clipboardList);
+    let listItem = document.createElement("li"),
+        listDiv = document.createElement("div"),
+        listSpan = document.createElement("span"),
+        listText = document.createTextNode(text);
+    listSpan.appendChild(listText)
+    listDiv.appendChild(listSpan)
+    listItem.appendChild(listDiv);
+    _clipboardList.appendChild(listItem);
+
+    listItem.addEventListener('click',(event)=>{
+        let {textContent} = event.target;
+        navigator.clipboard.writeText(textContent)
+        .then(()=>{console.log(`Text saved to clipboard`);
+            chrome.storage.local.get(['list'],clipboard=>{
+        let list = clipboard.list;
+        let ind = list.indexOf(textContent);
+        if (ind !== -1)
+            list.splice(ind,1);
+        list.unshift(textContent);
+        _clipboardList.innerHTML = "";
+        chrome.storage.local.set({'list':list},()=>getClipboardText());
+
+        });
+    })
+});
+}
+function getClipboardText(){
+	chrome.storage.local.get(['list'],clipboard=>{
+        let list = clipboard.list;
+        if (typeof list !== undefined)
+        list.forEach(item => addClipboardListItem(item));
+});
+}
+getClipboardText();
